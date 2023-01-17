c46b87ebcf9c1a82855a5ee3b3d0507626ed71e1
jxdoyle <56358030+jxdoyle@users.noreply.github.com>
2023-01-17 03:58:19 +0000

    updated game mechanics

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
-
 import Phaser from "phaser";
+import PlayScene from "./scenes/PlayScene";
+
+const WIDTH = 800;
+const HEIGHT = 600;
+const BIRD_POSITION = { x: WIDTH * 0.1, y: HEIGHT / 2 };
+
+const SHARED_CONFIG = {
+  width: WIDTH,
+  height: HEIGHT,
+  startPosition: BIRD_POSITION,
+};
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  ...SHARED_CONFIG,
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
-      gravity: { y: 200 }
-    }
+      debug: true,
+    },
   },
-  scene: {
-    preload: preload,
-    create: create
-  }
+  scene: [new PlayScene(SHARED_CONFIG)],
 };
 
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
